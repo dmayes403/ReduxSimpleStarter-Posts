bc065200a795e28fa03b5f6a30678c1abf89e6d4
dmayes403 <davidrmayes@gmail.com>
2017-08-26 14:51:25 -0600

    setting up postsIndex

--- src/index.js
+++ src/index.js
@@ -10,20 +10,11 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render () { return <div>Hello</div> }
-}
-
-class Goodbye extends React.Component {
-  render () { return <div>Goodbye</div> }
-}
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello}/>
-        <Route path="/goodbye" component={Goodbye}/>
+        {/* <Route path="" */}
       </div>
     </BrowserRouter>
   </Provider>
