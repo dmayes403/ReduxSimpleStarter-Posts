9287b61d7d99de596deca17354973053852f3a74
dmayes403 <davidrmayes@gmail.com>
2017-08-27 22:34:38 -0600

    setting up first end point

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axious';
+import API_Key from '../config.js';
+export const FETCH_POSTS = 'fetch_posts';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    }
+}
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export default '?key=PAPERCLIP1234'
