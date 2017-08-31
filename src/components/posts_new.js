16cdaa71762234f2d72befa0e78eb57964e10f9e
dmayes403 <davidrmayes@gmail.com>
2017-08-30 22:49:55 -0600

    working with forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // redux-form.com is an excellent source for form info
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     render() {
