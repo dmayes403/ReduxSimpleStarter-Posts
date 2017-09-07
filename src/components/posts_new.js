c46fd9f6754eaa9e687df616f60203e6d13eb307
dmayes403 <davidrmayes@gmail.com>
2017-09-06 22:10:42 -0600

    wiring up field input to redux

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 // redux-form.com is an excellent source for form info
+// use "Field" to wire up specific a field input in the component (it knows all of the event handlers)
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div>
+                <input
+                    type="text"
+                    // this wires up the event handlers to the between the input and the redux field
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return(
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
         )
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
